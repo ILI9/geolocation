16ba1d95821a43c18ea084480b457f2c747a9120
imuca <imuca@alpenite.com>
2020-01-10 23:33:10 +0100

    add air quality api, comment listen on port 3000

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ dataBase.loadDatabase();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.listen(3000, () => {
-    console.log(`running on port 3000`);
-});
+// app.listen(3000, () => {
+//     console.log(`running on port 3000`);
+// });
 
 
 
@@ -27,9 +27,20 @@ app.get('/tempo/:latlon', async function (req, res) {
     var latLong = req.params.latlon.split(',');
     var lat = latLong[0];
     var long = latLong[1];
-    var urlApi = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${long}`;
-    var fetch_response = await fetch(urlApi);
-    var data = await fetch_response.json();
+    var weather_url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${long}`;
+    var weather_resp = await fetch(weather_url);
+    var weather_data = await weather_resp.json();
+
+    var latBerlin = 52.520007;
+    var longBerlin = 13.404954;
+    var airQ_url = `https://api.openaq.org/v1/latest?coordinates=${latBerlin},${longBerlin}`;
+    var aq_resp = await fetch(airQ_url);
+    var aq_data = await aq_resp.json();
+
+    var data = {
+        weather: weather_data,
+        airQuality: aq_data
+    }
     res.json(data);
 });
 
